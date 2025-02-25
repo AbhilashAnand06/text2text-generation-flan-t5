1866d87d7a75b0197b065178a9c4115b6254c641
Abhilash Anand <51765299+AbhilashAnand06@users.noreply.github.com>
2025-02-25 18:11:12 +0100

    Add files via upload

---
+++ REAME.md
@@ -0,0 +1,4 @@
+# Text generation using Google flan-t5-small model
+
+This is a text generation application that is deployed in Hugging face using docker. You can find the app here: https://huggingface.co/spaces/ArcticM1n3r/text2text_with_docker
+
---
+++ app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from transformers import pipeline
+
+## create a FASTAPI app instance
+app = FastAPI()
+
+## Initialize the text generation pipeline
+pipe = pipeline("text2text-generation", model="google/flan-t5-small")
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
+
+# Define a function to handle the GET request at '/generate'
+@app.get("/generate")
+def generate(text:str):
+    # use the pipeline to generate text from the given input text
+    output = pipe(text)
+    # return the generated text in JSON response
+    return {"output": output[0]['generated_text']}
